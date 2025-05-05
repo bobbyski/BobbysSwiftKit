08d8ecd31a5bb80b728713dea6fe513b293f590f
Bobby Skinner <bobbyski@mac.com>
2025-05-04 20:31:46 -0400

    Initial Commit

---
+++ BobbysSwiftKit/BobbysSwiftKit.h
@@ -0,0 +1,18 @@
+//
+//  BobbysSwiftKit.h
+//  BobbysSwiftKit
+//
+//  Created by Bobby Skinner on 5/4/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BobbysSwiftKit.
+FOUNDATION_EXPORT double BobbysSwiftKitVersionNumber;
+
+//! Project version string for BobbysSwiftKit.
+FOUNDATION_EXPORT const unsigned char BobbysSwiftKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BobbysSwiftKit/PublicHeader.h>
+
+
---
+++ BobbysSwiftKitTests/BobbysSwiftKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  BobbysSwiftKitTests.swift
+//  BobbysSwiftKitTests
+//
+//  Created by Bobby Skinner on 5/4/25.
+//
+
+import Testing
+@testable import BobbysSwiftKit
+
+struct BobbysSwiftKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
